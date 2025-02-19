3e3ad27c8596019acd4a7a804fc7ee37e88db892
itskh4ng <itskh4ng@gmail.com>
2025-02-19 07:55:51 -0800

    Update theme.js

--- assets/theme.js
+++ assets/theme.js
@@ -7,7 +7,7 @@
             "--img": "url('https://i.pinimg.com/originals/49/e4/4b/49e44b6771c32e08e69d3f24040fd59d.gif')",
         },
         blue: {
-            "--img": "url('https://64.media.tumblr.com/1b7c6b1edc0f22df6dbbb0854c53db98/tumblr_o4h5q0HqPB1u7gnm9o1_500.gif')",
+            "--img": "url('https://4kwallpapers.com/images/walls/thumbs_3t/8228.jpg')",
         }
     };
 
