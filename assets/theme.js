483dc54db7029393155d7862a5ce43f8c1d5e215
itskh4ng <itskh4ng@gmail.com>
2025-02-19 17:19:33 -0800

    Update theme.js

--- assets/theme.js
+++ assets/theme.js
@@ -1,14 +1,7 @@
 (function () {
     const themes = {
-        cherry: {
-            "--img": "url('https://i.imgur.com/SAh2P1S.jpeg')",
-        },
-        white: {
-            "--img": "url('https://i.pinimg.com/originals/49/e4/4b/49e44b6771c32e08e69d3f24040fd59d.gif')",
-        },
-        blue: {
-            "--img": "url('https://4kwallpapers.com/images/walls/thumbs_3t/8228.jpg')",
-        }
+        cherry: { "--img": "url('https://i.imgur.com/SAh2P1S.jpeg')" },
+        blue: { "--img": "url('https://4kwallpapers.com/images/walls/thumbs_3t/8228.jpg')" }
     };
 
     function applyTheme(theme) {
@@ -16,13 +9,10 @@
             Object.entries(themes[theme]).forEach(([key, value]) => {
                 document.documentElement.style.setProperty(key, value);
             });
+            localStorage.setItem("theme", theme);
         }
     }
 
     const savedTheme = localStorage.getItem("theme");
-
-    if (savedTheme && themes[savedTheme]) {
-        applyTheme(savedTheme);
-    }
+    applyTheme(savedTheme && themes[savedTheme] ? savedTheme : "blue");
 })();
-
