ddefae340e981379f51391771005ccf5451ab197
itskh4ng-edu <phamj@pdxeaglestudent.org>
2025-03-04 09:45:09 -0800

    Update uv.config.js

--- uv/uv.config.js
+++ uv/uv.config.js
@@ -2,7 +2,7 @@ self.__uv$config = {
     prefix: '/service/',
 
     /* Bare server URL */ 
-    bare: 'https://bare-sigma.yourmom.eu.org/',
+    bare: 'https://tomp.app',
     
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
