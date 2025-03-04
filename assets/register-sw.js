13d79defa17ae7d854c5dd5bee6743466a246b1d
itskh4ng-edu <phamj@pdxeaglestudent.org>
2025-03-04 09:44:27 -0800

    Update register-sw.js

--- assets/register-sw.js
+++ assets/register-sw.js
@@ -1,6 +1,6 @@
 // The service worker registration script
 // This must run successfully before Ultraviolet is available to use
-const BARE_SERVER = "https://bare-sigma.yourmom.eu.org"
+const BARE_SERVER = "https://tomp.app"
 
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker.register('./uv-sw.js', {
