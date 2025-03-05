e9315256a77f08133e5330cd4681974764f9c255
itskh4ng <itskh4ng@gmail.com>
2025-03-04 23:08:51 -0800

    Update register-sw.js

--- assets/register-sw.js
+++ assets/register-sw.js
@@ -1,6 +1,6 @@
 // The service worker registration script
 // This must run successfully before Ultraviolet is available to use
-const BARE_SERVER = "https://tomp.app"
+const BARE_SERVER = "https://muddy-rice-ba72.acsessremote.workers.dev"
 
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker.register('./uv-sw.js', {
