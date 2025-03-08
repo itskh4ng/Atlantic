17c2013a8f7494ec2554a07bbe20e18ab81257b5
itskh4ng <itskh4ng@gmail.com>
2025-03-07 19:02:26 -0800

    Update uv.config.js

--- uv/uv.config.js
+++ uv/uv.config.js
@@ -2,7 +2,7 @@ self.__uv$config = {
     prefix: '/service/',
 
     /* Bare server URL */ 
-    bare: 'https://muddy-rice-ba72.acsessremote.workers.dev',
+    bare: 'https://nauka.politechnika-nova.edu.pl',
     
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
